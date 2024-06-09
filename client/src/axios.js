3e9bf7a4608649ff786b9075e810a8482d1df76a
unknown <tanquanga6k10@gmail.com>
2024-06-09 15:26:34 +0700

    update

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: 'https://new-sovo.v1r4l.site/api',
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://new-sovo.v1r4l.site/api',
+  // baseURL: 'http://localhost:8080/api/',
 
 
 });
