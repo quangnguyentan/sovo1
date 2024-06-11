a847f6f57365372313ac7063e78eabd6014f3315
Nguyen Tan Quang <tanquang1510@gmail.com>
2024-06-11 19:14:13 +0700

    update code

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: 'https://sovo2.link/api',
+  baseURL: 'https://xem1.sovo2.link/api',
   // baseURL: 'http://localhost:8080/api/',
 
 
